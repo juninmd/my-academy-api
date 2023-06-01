4a6044ac60514c79c93c476cea89036a1dd12254
Antonio Carlos <jr_acn@hotmail.com>
2023-06-01 02:00:58 -0300

    feat: logs

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log', 'verbose'],
+  });
 
   const config = new DocumentBuilder()
     .setTitle('My Academy')
