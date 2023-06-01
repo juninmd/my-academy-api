169cde40258a1b3c18c3f5c917a9b61cd5d3e51d
Antonio Carlos <jr_acn@hotmail.com>
2023-06-01 01:45:32 -0300

    feat: prisma debug

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: ["error", "info", "query", "warn"],
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
@@ -11,5 +17,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     this.$on('beforeExit', async () => {
       await app.close();
     });
+
   }
 }
