ccb6db59750f34016bc72739c331ab4ca1723dae
Antonio Carlos <jr_acn@hotmail.com>
2023-06-08 12:52:36 -0300

    feat: change date

--- src/workouts-sessions/dto/create-workouts-sessions.dto.ts
+++ src/workouts-sessions/dto/create-workouts-sessions.dto.ts
@@ -2,11 +2,12 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateWorkoutsSessionsDto {
   @ApiProperty({})
-  readonly id: number;
+  readonly id?: number;
   @ApiProperty({ required: true, description: 'Treino' })
   readonly workoutGroupId: number;
   @ApiProperty({
-    default: new Date().toJSON(),
+    type: Date,
+    default: new Date(),
   })
   readonly date: Date;
 
