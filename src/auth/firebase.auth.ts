2af89f77d1e53a230132a007a5b2e6bd879713a2
Antonio Carlos <jr_acn@hotmail.com>
2024-02-20 14:00:47 -0300

    feat: envs

--- src/auth/firebase.auth.ts
+++ src/auth/firebase.auth.ts
@@ -1,20 +1,9 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
 import * as firebase from 'firebase-admin';
-import * as serviceAccount from '../../service-account.json';
+import configs from '../configs';
 
-const firebase_params = {
-  type: serviceAccount.type,
-  projectId: serviceAccount.project_id,
-  privateKeyId: serviceAccount.private_key_id,
-  privateKey: serviceAccount.private_key,
-  clientEmail: serviceAccount.client_email,
-  clientId: serviceAccount.client_id,
-  authUri: serviceAccount.auth_uri,
-  tokenUri: serviceAccount.token_uri,
-  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-  clientC509CertUrl: serviceAccount.client_x509_cert_url
-}
+const firebase_params = configs.serviceAccount;
 
 @Injectable()
 export class PreauthMiddleware implements NestMiddleware {
@@ -34,10 +23,7 @@ export class PreauthMiddleware implements NestMiddleware {
     }
     try {
       const decodedToken = await this.defaultApp.auth().verifyIdToken(token.replace('Bearer ', ''));
-      const user = {
-        email: decodedToken.email
-      }
-      req['user'] = user;
+      req['user'] = decodedToken;
       next();
     } catch (error) {
       console.error(error);
---
+++ src/configs/index.ts
@@ -0,0 +1,14 @@
+export default {
+  serviceAccount: {
+    type: process.env.SA_TYPE,
+    projectId: process.env.SA_PROJECT_ID,
+    privateKeyId: process.env.SA_PRIVATE_KEY_ID,
+    privateKey: process.env.SA_PRIVATE_KEY,
+    clientEmail: process.env.SA_CLIENT_EMAIL,
+    clientId: process.env.SA_CLIENT_ID,
+    authUri: process.env.SA_AUTH_URI,
+    tokenUri: process.env.SA_TOKEN_URI,
+    authProviderX509CertUrl: process.env.SA_AUTH_PROVIDER,
+    clientX509CertUrl: process.env.SA_CLIENT_X509,
+  }
+}
