07ddb6d70f19abfb85ca9cfaecb73636eeedbffa
Antonio Carlos <jr_acn@hotmail.com>
2024-03-01 13:25:54 -0300

    Create update-repo.sh

---
+++ update-repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Defina as variáveis
+BRANCH="master"
+DOCKER_CONTAINER="my-academy-api"
+
+# Pare e remova o contêiner Docker existente
+sudo docker stop $DOCKER_CONTAINER && sudo docker rm $DOCKER_CONTAINER
+
+# Atualize o código
+git pull origin $BRANCH
+
+# Reconstrua e inicie o contêiner Docker
+docker run -p 80:80 -e PORT=80 -e DIRECT_URL=$DIRECT_URL -e DATABASE_URL=$DATABASE_URL my-academy-api --name $DOCKER_CONTAINER
